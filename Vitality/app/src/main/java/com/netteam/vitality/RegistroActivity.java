e4e21fcd65ac129b3f6e2039d6c985ec72622700
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2022-04-23 21:12:26 -0500

    Intento de Crear Registro de Usuario

--- Vitality/app/src/main/java/com/netteam/vitality/RegistroActivity.java
+++ Vitality/app/src/main/java/com/netteam/vitality/RegistroActivity.java
@@ -3,12 +3,49 @@ package com.netteam.vitality;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistroActivity extends AppCompatActivity {
+
+    EditText correo, contrasena;
+    Button registrar;
+    FirebaseAuth crear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        crear = FirebaseAuth.getInstance();
+
+        correo = findViewById(R.id.editTextTextPersonName);
+        contrasena = findViewById(R.id.editTextTextPassword);
+
+        registrar = findViewById(R.id.registrarBoton);
+
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registrarUsuario();
+            }
+        });
+    }
+
+    private void registrarUsuario(){
+        String correoU = correo.getText().toString();
+        String contra = contrasena.getText().toString();
+        if(TextUtils.isEmpty(correoU) && TextUtils.isEmpty(contra)){
+            Toast.makeText(this, "NO SE INGRESARON DATOS", Toast.LENGTH_SHORT).show();
+        }else{
+            //crear.createUserWithEmailAndPassword(correoU,contra)
+            Toast.makeText(this, "ESOOOOOOOOOOOOOOOOOOOOOOOOO", Toast.LENGTH_SHORT).show();
+        }
     }
 }
